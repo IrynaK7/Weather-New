b1d1b636b9c0b9106d485a39610c949a268d8ab6
IrynaK7 <117442616+IrynaK7@users.noreply.github.com>
2022-11-15 14:18:58 +0300

    Adding search engin

--- src/app.js
+++ src/app.js
@@ -53,9 +53,15 @@ function displayTemperature(response) {
   iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
-let apiKey = "6b6a30eeaf35bfabc392ab068b77991a";
-let city = "Warsaw";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-
-console.log(apiUrl);
-axios.get(apiUrl).then(displayTemperature);
+function search(city) {
+  let apiKey = "6b6a30eeaf35bfabc392ab068b77991a";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
